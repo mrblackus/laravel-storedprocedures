5183647d0784a760ca5999799e52ba39ed0a9ecd
Mathieu Savy <mathieu.savy@gmail.com>
2014-06-02 14:48:58 +0200

    Initializing laravel package

---
+++ src/Mrblackus/LaravelStoredprocedures/LaravelStoredproceduresServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Mrblackus\LaravelStoredprocedures;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelStoredproceduresServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('mrblackus/laravel-storedprocedures');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
